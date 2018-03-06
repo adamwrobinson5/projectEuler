88ebc23a36deac207dc64c445a11a404102369bb
Adam Robinson <adamwrobinson5@gmail.com>
2018-03-06 12:00:22 -0500

    Finished Problem 9

--- projectEuler.py
+++ projectEuler.py
@@ -244,11 +244,41 @@ import math
 
 # # ***** PROBLEM 9 *****
 # # A Pythagorean triplet is a set of three natural numbers, a < b < c, 
-# # for which, a2 + b2 = c2
-# # For example, 32 + 42 = 9 + 16 = 25 = 52.
+# # for which, a**2 + b**2 = c**2
+# # For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
 # # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-# # Find the product abc.
+# # Find the product a * b * c.
+
+# print ''
+# print '*** PROBLEM 9 ***'
+# print ''
+# def triplet():
+#     done = False
+#     for c in range(3,1000):
+#         for b in range(2, c):
+#             for a in range(1, b):
+#                 sum = a + b + c
+#                 if sum > 1000:
+#                     break
+#                 if sum == 1000:
+#                     if a**2 + b**2 == c**2:
+#                         triplet = a * b * c
+#                         done = True
+#                     if done == True:
+#                         print 'end a: ' + str(a)
+#                         print 'end b: ' + str(b)
+#                         print 'end c: ' + str(c)
+#                         print 'triplet: ' + str(triplet)
+#                         return
+# triplet()
+
+
+
+
+# # ***** PROBLEM 10 *****
+# # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# # Find the sum of all the primes below two million.
 
 print ''
-print '*** PROBLEM 9 ***'
+print '*** PROBLEM 10 ***'
 print ''
